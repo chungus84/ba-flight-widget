50db515d02e61bd88e6d4d27e0a9ad2666f53d58
kenichi beveridge <kenbev84@gmail.com>
2023-04-06 13:48:15 +0100

    Added comments

--- client/app.js
+++ client/app.js
@@ -1,6 +1,9 @@
+/* looks for the table-body id on index.html */
 const tableBody = document.getElementById('table-body');
 
+
 const getFlight = () => {
+  /* Method to grab the flight data from localhost:8000/flights */
   fetch('http://localhost:8000/flights')
     .then(response => response.json())
     .then(flights => {
@@ -13,12 +16,15 @@ const getFlight = () => {
 getFlight();
 
 const populateTable = (flights) => {
+  /* method to populate the table on index.html takes a list and loops through it */
+
   for (const flight of flights) {
     const tableRow = document.createElement('tr');
     const tableIcon = document.createElement('td');
     tableIcon.textContent = '✈';
     tableRow.append(tableIcon);
 
+    /*creates a flight detail object */
     const flightDetails = {
 
       time: new Date(flight[11] * 1000).toLocaleTimeString("en-UK").slice(0, 5),
@@ -26,6 +32,9 @@ const populateTable = (flights) => {
       destination: flight[13],
       flight: flight[17]
     }
+
+    /* Nested loop to  loop through flightDetail object to create an array which will
+    make it look like a tile on a flipper board you get at airports */
     for (const flightDetail in flightDetails) {
       const tableCell = document.createElement('td');
       const word = Array.from(flightDetails[flightDetail])
@@ -33,6 +42,7 @@ const populateTable = (flights) => {
       for (const [index, letter] of word.entries()) {
         const letterElement = document.createElement('div');
 
+        /* Timeout to add a delay to the table showing for each element that is displayed on index.html */
         setTimeout(() => {
           letterElement.classList.add('flip');
           letterElement.textContent = letter;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* Server setup which will call flight radar api */
 const PORT = 8000;
 const axios = require("axios");
 const express = require('express');
@@ -6,7 +7,7 @@ const cors = require('cors')
 require('dotenv').config();
 app.use(cors())
 
-
+/* sets path to flights and will return all fligths for British Airways */
 app.get('/flights', (req, res) => {
   const options = {
     method: 'GET',
@@ -17,7 +18,6 @@ app.get('/flights', (req, res) => {
       'X-RapidAPI-Host': 'flight-radar1.p.rapidapi.com'
     }
   };
-
 
 
   axios.request(options).then(function (response) {
